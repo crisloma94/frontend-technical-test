ad07b582f9da31430ed0f3432cb44f22686f514a
Cristina López <crisloma94@gmail.com>
2022-06-05 13:58:27 +0200

    Add favorites page

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,19 @@
-export default function FavoritesPage() {
+import MeetupItem from "../components/meetups/MeetupItem";
+import classes from "./../components/meetups/MeetupList.module.css";
+
+export default function FavoritesPage({favorites, setFavorites}) {
   return (
     <section>
-      <h1>Favorites Page</h1>
-    </section>
+    <h1>Favorites Page</h1>
+    <ul className={classes.list}>
+      {favorites.map((meetupItemData) => 
+        <MeetupItem 
+          item={meetupItemData} 
+          key={meetupItemData.id}
+          isFavorite={!!favorites.find((fav) => fav.id === meetupItemData.id)} 
+          setFavorites={setFavorites}/>
+      )}
+    </ul>
+  </section>
   );
 }
